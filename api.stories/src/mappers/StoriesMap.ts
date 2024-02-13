519b734681140333bf8060cdb7e3eef86002eeb1
Animila <khristoforov-i@mail.ru>
2024-02-13 16:43:00 +0900

    prod

--- api.stories/src/mappers/StoriesMap.ts
+++ api.stories/src/mappers/StoriesMap.ts
@@ -1,12 +1,13 @@
 import {Storie as PersistanceStorie} from "@prisma/client";
 import {Storie} from "../domain/Storie";
+import {UniqueEntityID} from "../../support/domain/UniqueEntityID";
 
 export class StoriesMap {
     public static toDomain(raw: PersistanceStorie): Storie | null {
         const storieOrError = Storie.create({
             background: raw.background,
             type: raw.type
-        })
+        },new UniqueEntityID(raw.id))
         if(storieOrError.isFailure) {
             console.log(storieOrError.errorValue())
             return null
